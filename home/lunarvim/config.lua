99b39dd02c7ebd7e864dd45f4cef812be587855d
Szymon Lanucha <slann@protonmail.com>
2026-04-22 22:21:47 +0200

    Added marks indicators for lunarvim

--- home/lunarvim/config.lua
+++ home/lunarvim/config.lua
@@ -1,6 +1,28 @@
 lvim.plugins = {
   { "miikanissi/modus-themes.nvim", priority = 1000 },
-  { "tpope/vim-fugitive" }
+
+  { "tpope/vim-fugitive" },
+
+  {
+    "chentoast/marks.nvim",
+    config = function()
+      require("marks").setup({
+        -- Whether to set default mappings, such as m[a-z], dm[a-z]
+        default_mappings = true,
+        -- Which builtin marks to show (e.g. '.', '^')
+        builtin_marks = { ".", "<", ">", "^" },
+        -- Refresh interval in milliseconds
+        refresh_interval = 250,
+        -- Sign priorities for the gutter
+        sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
+        bookmark_1 = {
+          sign = "", -- You can use any icon here
+          virt_text = "Bookmark",
+          annotate = false,
+        }
+      })
+    end
+  }
 }
 
 lvim.colorscheme = "modus_operandi"
